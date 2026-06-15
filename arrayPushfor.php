2ee596d1cf0cfbce9b71767b650bdc6693eb9c4f
josephfranca <116111572+josephfranca@users.noreply.github.com>
2026-06-15 13:50:35 -0300

    Add files via upload

---
+++ arrayPushfor.php
@@ -0,0 +1,13 @@
+<?php
+    //Crie um array de 1 a 10
+    //utilize um loop para criar este array
+    //imprima o array criado com print_r
+
+    $arr = [];
+    for($i = 1; $i <=10; $i++){
+        array_push($arr,$i);
+        // print_r($arr);
+    }
+
+    print_r($arr);
+?>
